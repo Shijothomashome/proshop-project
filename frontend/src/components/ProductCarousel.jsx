0c89344227655cc029cb69abfdd77d2df9f26d5e
shijo thomas <shijothomashome@gmail.com>
2024-04-08 20:48:17 +0530

    final commit

--- frontend/src/components/ProductCarousel.jsx
+++ frontend/src/components/ProductCarousel.jsx
@@ -15,7 +15,8 @@ const ProductCarousel = () => {
       {products.map((product) => (
         <Carousel.Item key={product._id}>
           <Link to={`/product/${product._id}`}>
-            <Image src={`${CLOUDINARY_IMG_URL}/${product.image}`} alt={product.name} fluid />
+            <Image src={`${CLOUDINARY_IMG_URL}/${product.image}`} className="carousel-img" alt={product.name} fluid />
+            <p className="carousel-description d-none d-lg-block">{`"${product.description}"`}</p>
 
             <Carousel.Caption className="carousel-caption ">
               <h2>
@@ -31,3 +32,38 @@ const ProductCarousel = () => {
 };
 
 export default ProductCarousel;
+
+// import Carousel from 'react-bootstrap/Carousel';
+// import ExampleCarouselImage from 'components/ExampleCarouselImage';
+
+// function CarouselFadeExample() {
+//   return (
+//     <Carousel fade>
+//       <Carousel.Item>
+//         <ExampleCarouselImage text="First slide" />
+//         <Carousel.Caption>
+//           <h3>First slide label</h3>
+//           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+//         </Carousel.Caption>
+//       </Carousel.Item>
+//       <Carousel.Item>
+//         <ExampleCarouselImage text="Second slide" />
+//         <Carousel.Caption>
+//           <h3>Second slide label</h3>
+//           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+//         </Carousel.Caption>
+//       </Carousel.Item>
+//       <Carousel.Item>
+//         <ExampleCarouselImage text="Third slide" />
+//         <Carousel.Caption>
+//           <h3>Third slide label</h3>
+//           <p>
+//             Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+//           </p>
+//         </Carousel.Caption>
+//       </Carousel.Item>
+//     </Carousel>
+//   );
+// }
+
+// export default CarouselFadeExample;
