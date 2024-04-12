8a3e393e7275ed6676aba50f43b4de312aaedfe1
shijo thomas <shijothomashome@gmail.com>
2024-04-12 14:49:09 +0530

    modified

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 import path from "path";
 import express from "express";
 import dotenv from "dotenv";
-dotenv.config(); // should call before ontop of using any env variable
 import cookieParser from "cookie-parser";
 import connectDB from "./config/db.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
@@ -9,11 +8,15 @@ import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
 import uploadRoutes from "./routes/uploadRoutes.js";
+
+dotenv.config(); // Should call before using any env variable
+
 const port = process.env.PORT || 5000;
 
-connectDB(); //Connect to MongoDB
+connectDB(); // Connect to MongoDB
 
 const app = express();
+
 // Body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -30,21 +33,24 @@ app.get("/api/config/paypal", (req, res) => {
   res.send({ clientId: process.env.PAYPAL_CLIENT_ID });
 });
 
-const __dirname = path.resolve();
-
+// Serving static files
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
-  // Any route that is not api will be redirected to index.html
+  // Set the build directory
+  app.use(express.static(path.resolve("frontend", "build")));
+  // Serve index.html for any other routes
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+    res.sendFile(path.resolve("frontend", "build", "index.html"))
   );
 } else {
+  // In development mode, serve a message for root route
   app.get("/", (req, res) => {
     res.send("API is running....");
   });
 }
 
+// Error handling middleware
 app.use(notFound);
 app.use(errorHandler);
 
+// Start the server
 app.listen(port, () => console.log(`Server is running on port ${port}`));
