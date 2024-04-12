423b89b8f91032d8fb77ecdef7cdbb19559281e7
shijo thomas <shijothomashome@gmail.com>
2024-04-12 14:42:05 +0530

    modified

--- backend/server.js
+++ backend/server.js
@@ -32,11 +32,17 @@ app.get("/api/config/paypal", (req, res) => {
 
 const __dirname = path.resolve();
 
-
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  // Any route that is not api will be redirected to index.html
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
   app.get("/", (req, res) => {
     res.send("API is running....");
   });
-
+}
 
 app.use(notFound);
 app.use(errorHandler);
