d8719ee128142e662d99f0f2d843b3a614173ee0
shijo thomas <shijothomashome@gmail.com>
2024-04-12 15:10:09 +0530

    modified

--- backend/server.js
+++ backend/server.js
@@ -38,7 +38,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/var/task/frontend/build")));
   // Any route that is not api will be redirected to index.html
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname,"var", 'task', "frontend", "build", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
